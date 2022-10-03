74af315e0663871ce139b88379e59ce259289080
ellisvelandia <eyis619@gmail.com>
2022-10-03 17:28:25 -0500

    dad

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const CARDS = 3;
 
 for (let i = 0; i < CARDS; i++) {
   let id = getRandom(150);
-  searchPokemonById(150);
+  searchPokemonById(id);
 }
 
 function getRandom(max) {
@@ -10,15 +10,19 @@ function getRandom(max) {
 }
 
 let draggableElements = document.querySelector(".draggable-elements");
-console.log(draggableElements);
 
 let pokemonSearched = [];
 async function searchPokemonById(id) {
-  const res = await fetch(`https://pokeapi.co/api/v2/ability/${id}`);
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
   const data = await res.json();
   pokemonSearched.push(data);
 
+  draggableElements.innerHTML = "";
   pokemonSearched.forEach((pokemon) => {
     console.log(pokemon);
+    draggableElements.innerHTML += `
+    <div class="pokemon">
+       <img class="image" src="${pokemon.sprites.other["dream_world"].front_default}"  alt="pokemon"/>
+    </div>`;
   });
 }
